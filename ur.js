e52f9d89eacd63bda87a091d1594eea29e99360e
Grejfreak <80950560+Grejfreak@users.noreply.github.com>
2021-03-19 10:53:04 +0100

    Update ur.js

--- ur.js
+++ ur.js
@@ -1,7 +1,8 @@
-
 // Set the date we're counting down to
 var countDownDate = new Date("May 16, 2021 00:00:00").getTime();
-
+var outdatedDate =  new Date("Jul 15, 2021 00:00:00").getTime();
+const jagtNotStarted = "Bukkejagt starter om: "
+const jagtStarted = "Bukkejagt er startet!"
 // Update the count down every 1 second
 var x = setInterval(function() {
 
@@ -10,7 +11,8 @@ var x = setInterval(function() {
     
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-    
+    var outdatedDistance = outdatedDate - now;
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -20,12 +22,17 @@ var x = setInterval(function() {
   // Output the result in an element with id="demo"
   document.getElementById("countdown").innerHTML = days + "d " + hours + "t "
   + minutes + "m " + seconds + "s ";
-    
+  document.getElementById("countdownText").innerHTML = jagtNotStarted;
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "NU";
+    document.getElementById("countdown").style.display = "none";
+    document.getElementById("countdownText").innerHTML = jagtStarted;
+  }
+
+  if (now >= outdatedDate){
+    document.getElementById("countdown").style.display = "none";
+    document.getElementById("countdownText").innerHTML = "Bukkejagten er SLUT";
   }
 }, 1000);
-
 
